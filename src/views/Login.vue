fddb24db7204624514ddb501ddfab79c207bb161
Ben Hession <bshession@gmail.com>
2021-04-04 17:29:14 +0100

    Gracefully handle login errors. Add styling.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -12,21 +12,23 @@
         </ion-toolbar>
       </ion-header>
 
+      <div class="login-form-container">
+        <div class="login-form">
+          <ion-list>
+            <ion-item>
+              <ion-label color="primary" position="floating">Username</ion-label>
+              <ion-input v-model="username"/>
+            </ion-item>
 
-      <ion-list>
-        <ion-item>
-          <ion-label color="primary" position="floating">Username</ion-label>
-          <ion-input v-model="username"/>
-        </ion-item>
+            <ion-item>
+              <ion-label color="primary" position="floating">Password</ion-label>
+              <ion-input v-model="password" type="password"/>
+            </ion-item>
+          </ion-list>
 
-        <ion-item>
-          <ion-label color="primary" position="floating">Password</ion-label>
-          <ion-input v-model="password" type="password"/>
-        </ion-item>
-      </ion-list>
-
-      <ion-button @click="logIn()">Login</ion-button>
-
+          <ion-button class="login-btn" @click="logIn()">Login</ion-button>
+        </div>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -48,6 +50,7 @@ import {ref} from 'vue';
 
 import {ACTIONS, useStore} from '@/store'
 import {useRouter} from "vue-router";
+import {presentMessageAlert} from "@/alerts/messageAlert";
 
 export default {
   name: "Login",
@@ -58,32 +61,60 @@ export default {
     const router = useRouter()
     const store = useStore();
 
+    const messageAlert = (header: string, message: string) => presentMessageAlert(store, router, header, message);
+
     const username = ref("");
     const password = ref("");
 
+    // if logged in push to tab1 view
     const loggedInPromise: Promise<boolean> = store.getters.getLoggedIn
     loggedInPromise.then((loggedIn) => {
       if (loggedIn) {
         router.push({name: 'tab1'})
       }
-    });
+    }).catch(reason => console.error(reason));
 
+    // login and get auth tokens, handle errors gracefully
     function logIn() {
       store.dispatch(ACTIONS.LOG_IN, [username.value, password.value])
           .then(() => {
-            console.log(store.getters.getLoggedIn);
             router.push({name: 'tab1'})
           })
-          .catch((error) => console.log(error));
+          .catch((e) => {
+            if (e instanceof Error) {
+              console.error(e);
+              if (e.message === 'Network Error') {
+                messageAlert('Network Error', 'Unable to connect to authorisation server');
+              } else if (e.message === 'Request failed with status code 401') {
+                messageAlert('Login failed', 'Please provide a valid username and password');
+              } else {
+                messageAlert('Unknown error', 'see console for details');
+              }
+            }
+          });
     }
 
     return {router, store, username, password, logIn};
   }
 }
-;
 
 </script>
 
 <style scoped>
+.login-form-container {
+  height: 50%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.login-form {
+  width: 100%;
+}
+
+.login-btn {
+  width: 100%;
+}
 
 </style>
