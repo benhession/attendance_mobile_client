66cacfe27b72d17495391af7f00b0b38a28d670b
Ben Hession <bshession@gmail.com>
2021-04-11 18:05:00 +0100

    Create a service to query the student REST API endpoint.

---
+++ src/services/universityClassService.ts
@@ -0,0 +1,21 @@
+import axios, {AxiosRequestConfig, AxiosResponse} from "axios";
+
+const webServiceStudentClasses = axios.create({
+    baseURL: 'http://192.168.0.13:8080/student',
+})
+
+export default {
+    fetchStudentClasses(accessToken: string): Promise<AxiosResponse> {
+
+        const token = 'Bearer '.concat(accessToken);
+
+        const config: AxiosRequestConfig = {
+            headers: {
+                Authorization: token,
+                Accept: "application/json, text/plain"
+            }
+        }
+
+        return webServiceStudentClasses.get("/classes", config);
+    }
+}
