175ff78949f845643616b34e23c0822cc3c4e2a0
Ben Hession <bshession@gmail.com>
2021-05-09 10:37:24 +0100

    Disable Y-Scroll

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,7 +5,7 @@
         <ion-title>Login</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
+    <ion-content :fullscreen="true" :scroll-y="false">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Login</ion-title>
