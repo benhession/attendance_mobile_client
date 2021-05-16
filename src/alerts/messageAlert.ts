ac8a3437730c231eba9c3fcb286f7b6f907538ee
Ben Hession <bshession@gmail.com>
2021-05-16 14:03:01 +0100

    Remove unnecessary imports.

--- src/alerts/messageAlert.ts
+++ src/alerts/messageAlert.ts
@@ -1,9 +1,6 @@
 import {alertController} from "@ionic/vue";
-import {Router} from "vue-router";
-import {Store} from "vuex";
-import {State} from "@/store";
 
-export async function presentMessageAlert(store: Store<State>, router: Router, header: string, message: string) {
+export async function presentMessageAlert(header: string, message: string) {
     const alert = await alertController.create({
         header: header,
         message: message,
